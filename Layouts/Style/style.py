af51b9ea13ab71e0b45eb43cdf9b1cb063463d2d
moritztuhh <moritz.haussner@tuhh.de>
2024-03-21 21:32:22 +0100

    Style Update

--- Layouts/Style/style.py
+++ Layouts/Style/style.py
@@ -2,23 +2,32 @@
 def GetStyleStartPage():
     # Find a way to access button
     return """
+        //Header
         h1 {
             text-align: center;
             display:block;
             margin-top: 1em;
             font-size: 10em;
         }
+        // Markdown
         p {
             text-align: center;
             font-size: 3em;
             display:block; 
         }
-        button {
-            background-color: #04AA6D;
-            width: 50%
+        //a {
+        //    background-color: #04AA6D;
+        //    width: 50%;
+        //    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+        //}
+        #start_button {
+            margin: auto;
+            width: 50%;
             box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
         }
         """
+
+
 def GetStyleRulesPage():
     # Find a way to access button
     return """
@@ -30,7 +39,7 @@ def GetStyleRulesPage():
         }
         p {
             text-align: center;
-            font-size: 3em;
+            font-size: 1em;
             display:block; 
         }
         button {
@@ -39,6 +48,8 @@ def GetStyleRulesPage():
             box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
         }
         """
+
+
 def GetStyleTestsPage():
     # Find a way to access button
     return """
@@ -46,11 +57,11 @@ def GetStyleTestsPage():
             text-align: center;
             display:block;
             margin-top: 1em;
-            font-size: 10em;
+            font-size: 5em;
         }
         p {
             text-align: center;
-            font-size: 3em;
+            font-size: 1.5em;
             display:block; 
         }
         button {
@@ -59,6 +70,8 @@ def GetStyleTestsPage():
             box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
         }
         """
+
+
 def GetStyleScorePage():
     # Find a way to access button
     return """
@@ -79,6 +92,8 @@ def GetStyleScorePage():
             box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
         }
         """
+
+
 def GetStyleRecommendationPage():
     # Find a way to access button
     return """
@@ -99,6 +114,8 @@ def GetStyleRecommendationPage():
             box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
         }
         """
+
+
 def GetStyleEstimationPage():
     # Find a way to access button
     return """
--- Layouts/start_page.py
+++ Layouts/start_page.py
@@ -3,10 +3,12 @@ import Layouts.Style.style as st
 
 # TODO Looks
 
+
 def getLayoutStart() -> gr.Blocks:
     with gr.Blocks(theme=gr.themes.Soft(), css=st.GetStyleStartPage()) as layout:
         gr.Markdown("""# Learning Finish""")
         gr.Markdown("""The assisted finnish-learning experience""")
-        button = gr.Button(value="New Game", link="/rules")
+        button = gr.Button(value="New Game", link="/rules",
+                           elem_id="start_button")
     layout.queue()
     return layout
