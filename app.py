6dfb804ae527ffc9603508258a72d0d4af957e02
Lucas Martens <lucas.martens@tuhh.de>
2024-03-21 09:36:50 +0100

    added placeholder pages for score, estimation and recommendations with pathing

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def getLayoutStart() -> gr.Blocks:
     layout.queue()
     return layout
 
-
 # TODO: Logic and Look
 def getLayoutRules() -> gr.Blocks:
     with gr.Blocks() as layout:
@@ -45,15 +44,57 @@ def getLayoutTests() -> gr.Blocks:
     with gr.Blocks() as layout:
         gr.Markdown("""# Tests 
                     """) #TODO add test count/progress
+    #TODO add submit button
+        button = gr.Button(value="continue", link="/score") #Only for path-testing!
     layout.queue()
     return layout
 
+#TODO: Logic and Look
+def getLayoutScore() -> gr.Blocks:
+    with gr.Blocks() as layout:
+        gr.Markdown("""# your score is: 
+                    """)
+        button = gr.Button(value="continue", link="/estimation")
+    layout.queue()
+    return layout
+
+#TODO: Logic and Look
+def getLayoutEstimation() -> gr.Blocks:
+    with gr.Blocks() as layout:
+        gr.Markdown("""# Estimated cards you have chosen: 
+                    """)
+        #TODO add dynamic space for case-display (The number of cases may vary!!!)
+    
+        with gr.Row():
+            bbutton = gr.Button(value="go back", link="/score")
+            cbutton = gr.Button(value="see recommendations", link="/recommendations")
+    layout.queue()
+    return layout
+
+#TODO: Look and Logic
+def getLayoutRecommendation() -> gr.Blocks:
+    with gr.Blocks() as layout:
+        gr.Markdown("""# Recommendations
+                    """)
+        #TODO add dynamic space for case-display (The number of cases may vary!!!)
+        #TODO what if no recommendations?
+        with gr.Row():
+            bbutton = gr.Button(value="quit", link="/start") #TODO delete cache of "last" player
+            cbutton = gr.Button(value="redo tests", link="/tests") #does redoing the tests even make sense?
+    layout.queue()
+    return layout
 
 #TODO add remaining paths
 start_screen = getLayoutStart()
 rule_screen = getLayoutRules()
 tests_screen = getLayoutTests()
+score_screen = getLayoutScore()
+esti_screen = getLayoutEstimation()
+recomm_screen = getLayoutRecommendation()
 gr.mount_gradio_app(app=app, blocks=start_screen, path="/start")
 gr.mount_gradio_app(app=app, blocks=rule_screen, path="/rules")
 gr.mount_gradio_app(app=app, blocks=tests_screen, path="/tests")
+gr.mount_gradio_app(app=app, blocks=score_screen, path="/score")
+gr.mount_gradio_app(app=app, blocks=esti_screen, path="/estimation")
+gr.mount_gradio_app(app=app, blocks=recomm_screen, path="/recommendations")
 # Maybe into function
