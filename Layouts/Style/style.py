b434901b735dda78ba52261192ed7e32d217d7c8
moritztuhh <moritz.haussner@tuhh.de>
2024-03-21 13:33:13 +0100

    addet style file but doesnt work yet

---
+++ Layouts/Style/style.py
@@ -0,0 +1,11 @@
+def GetStyleStartPage():
+    return """
+        h1 {
+            text-align: center;
+            display:block;
+        }
+        h2 {
+            text-align: center;
+            display:block;
+        }
+        """
--- Layouts/start_page.py
+++ Layouts/start_page.py
@@ -1,10 +1,15 @@
 import gradio as gr
+from Style.style import GetStyleStartPage
 
-#TODO Looks and Logic
+style = GetStyleStartPage()
+
+# TODO Looks and Logic
+
+
 def getLayoutStart() -> gr.Blocks:
-    with gr.Blocks() as layout:
+    with gr.Blocks(theme=gr.themes.Soft(), css=style) as layout:
         gr.Markdown("""# Learning Finish""")
         gr.Markdown("""The assisted finnish-learning experience""")
-        button = gr.Button(value= "New Game", link="/rules")
+        button = gr.Button(value="New Game", link="/rules")
     layout.queue()
     return layout
